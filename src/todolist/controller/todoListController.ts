55c09085836f88c5d258e262ee612132f43a3749
Petri Silen <petri.silen@silensoft.com>
2020-02-05 14:36:41 +0200

    Update todoListController.ts

--- src/todolist/controller/todoListController.ts
+++ src/todolist/controller/todoListController.ts
@@ -2,10 +2,14 @@ import addTodo from '@/todolist/model/actions/addTodo';
 import removeAllTodos from '@/todolist/model/actions/removeAllTodos';
 
 export default {
-  handleKeyPress(keyboardEvent: KeyboardEvent): void {
+  handleKeyDown(keyboardEvent: KeyboardEvent): void {
     if (keyboardEvent.code === 'KeyA' && keyboardEvent.ctrlKey) {
+      keyboardEvent.stopPropagation();
+      keyboardEvent.preventDefault();
       addTodo();
     } else if (keyboardEvent.code === 'KeyR' && keyboardEvent.ctrlKey) {
+      keyboardEvent.stopPropagation();
+      keyboardEvent.preventDefault();
       removeAllTodos();
     }
   }
